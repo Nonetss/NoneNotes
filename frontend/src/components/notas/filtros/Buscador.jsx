5d41e0cfcad9a26228e439b398d8c799294b107c
Nonetss <amorenolopezbarajas@gmail.com>
2024-12-26 21:28:28 +0100

    Hacemos un componente buscador

---
+++ frontend/src/components/notas/filtros/Buscador.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import "./global.css";
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    onSearch(event.target.value); // Llama a la función onSearch con el nuevo término
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Buscar carpetas y notas..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <button className="lupita">
+        <span>🔍</span>
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
