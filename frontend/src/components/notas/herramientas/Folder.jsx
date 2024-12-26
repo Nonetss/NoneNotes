a78ea7297af329aeef40a57209ca94c5fec713c1
Nonetss <amorenolopezbarajas@gmail.com>
2024-12-26 18:56:52 +0100

    Sacamos el componente fuera

--- frontend/src/components/notas/herramientas/Folder.jsx
+++ frontend/src/components/notas/herramientas/Folder.jsx
@@ -1,11 +1,15 @@
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { createBlendy } from "blendy";
+import api from "@/utils/api";
+import FolderOptions from "./FolderOptions"; // Importa el nuevo componente
 import "./global.css";
 
 const NewFolder = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
-  const [folderName, setFolderName] = useState(""); // Estado para el nombre de la carpeta
+  const [folderName, setFolderName] = useState("");
+  const [folderParent, setFolderParent] = useState("");
+  const [folders, setFolders] = useState([]);
   const blendyRef = useRef(null);
 
   useEffect(() => {
@@ -16,27 +20,70 @@ const NewFolder = () => {
     blendyRef.current.update();
   }, []);
 
+  useEffect(() => {
+    const fetchFolders = async () => {
+      try {
+        const response = await api.get("/folders/");
+        const uniqueFolders = response.data.filter(
+          (folder, index, self) =>
+            index === self.findIndex((f) => f.id === folder.id),
+        );
+        setFolders(uniqueFolders);
+      } catch (error) {
+        console.error("Error fetching folders:", error);
+      }
+    };
+
+    fetchFolders();
+  }, []);
+
   const openModal = () => {
-    setIsAnimating(true); // Activar la animación
-    setIsModalOpen(true); // Mostrar la modal
+    setIsAnimating(true);
+    setIsModalOpen(true);
     blendyRef.current.toggle("new-folder", () => {
-      setIsAnimating(false); // Finalizar la animación
+      setIsAnimating(false);
     });
   };
 
   const closeModal = () => {
-    setIsAnimating(true); // Activar la animación
+    setIsAnimating(true);
     blendyRef.current.untoggle("new-folder", () => {
-      setIsAnimating(false); // Finalizar la animación
-      setIsModalOpen(false); // Cerrar la modal
+      setIsAnimating(false);
+      setIsModalOpen(false);
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Nombre de la carpeta:", folderName);
-    setFolderName(""); // Reiniciar el campo de entrada
-    closeModal(); // Cerrar la modal después de enviar
+
+    if (!folderName.trim()) {
+      alert("Por favor, ingrese un nombre para la carpeta.");
+      return;
+    }
+
+    try {
+      const parentId = folderParent === "" ? null : parseInt(folderParent, 10);
+
+      const response = await api.post("/folders/", {
+        nombre: folderName,
+        parent: parentId,
+      });
+      console.log("Carpeta creada:", response.data);
+
+      setFolders((prevFolders) => [...prevFolders, response.data]);
+
+      setFolderName("");
+      setFolderParent("");
+      closeModal();
+    } catch (error) {
+      console.error(
+        "Error al crear la carpeta:",
+        error.response?.data || error.message,
+      );
+      alert(
+        "Ocurrió un error al crear la carpeta. Por favor, intenta nuevamente.",
+      );
+    }
   };
 
   return (
@@ -49,31 +96,37 @@ const NewFolder = () => {
         <p>Nueva carpeta</p>
       </div>
 
-      {/* Modal */}
       {(isModalOpen || isAnimating) && (
         <div
           className={`modal-overlay ${isModalOpen ? "active" : ""}`}
           data-blendy-to="new-folder"
           onClick={closeModal}
         >
-          <div
-            className="modal-contenido"
-            onClick={(e) => e.stopPropagation()} // Evitar cerrar al hacer clic dentro de la modal
-          >
+          <div className="modal-contenido" onClick={(e) => e.stopPropagation()}>
             <button className="cerrar-button" onClick={closeModal}>
               &times;
             </button>
             <h2>Crear Nueva Carpeta</h2>
             <form onSubmit={handleSubmit}>
-              <div className="new-folder-form-group">
+              <div className="form-group">
                 <label htmlFor="folderName">Nombre de la carpeta:</label>
                 <input
                   type="text"
                   id="folderName"
+                  autoComplete="off"
                   value={folderName}
                   onChange={(e) => setFolderName(e.target.value)}
                   required
                 />
+                <label htmlFor="folderParent">Seleccionar carpeta padre:</label>
+                <select
+                  id="folderParent"
+                  value={folderParent}
+                  onChange={(e) => setFolderParent(e.target.value)}
+                >
+                  <option value="">/</option>
+                  <FolderOptions folders={folders} />
+                </select>
               </div>
               <button type="submit" className="submit-button">
                 Crear Carpeta
