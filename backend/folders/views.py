6cc88bff7de2d3031394c64db072f68aa4c659ca
Nonetss <amorenolopezbarajas@gmail.com>
2024-12-28 17:36:57 +0100

    Cambiamos la view

--- backend/folders/views.py
+++ backend/folders/views.py
@@ -10,13 +10,8 @@ class FolderViewSet(ModelViewSet):
     serializer_class = FolderSerializer
 
     def get_queryset(self):
-        user = self.request.user
-        if self.action == "list":
-            # Solo carpetas raíz para la acción de listado
-            return Folder.objects.filter(parent=None, usuario=user)
-        else:
-            # Todas las carpetas del usuario para otras acciones
-            return Folder.objects.filter(usuario=user)
+        # Incluir todas las carpetas asociadas al usuario autenticado
+        return Folder.objects.filter(usuario=self.request.user)
 
     def perform_create(self, serializer):
         # Asignar automáticamente el usuario autenticado
